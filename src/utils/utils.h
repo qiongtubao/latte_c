d788db3bb52667f7e39cc07e97a10f93590236c4
qiongtubao <810776886@qq.com>
2022-12-28 02:04:45 +0800

    add utils module(ll2string and string2ll)

--- src/utils/utils.h
+++ src/utils/utils.h
@@ -1,6 +1,8 @@
 #include <stdio.h>
-
+#include <math.h>
+#include <stdint.h>
 int ll2string(char *s, size_t len, long long value);
+int string2ll(const char *s, size_t slen, long long *value);
 // int string2ll(const char *s, size_t slen, long long *value);
 // int string2ull(const char *s, unsigned long long *value);
 // int string2l(const char *s, size_t slen, long *value);
--- src/utils/utils_test.c
+++ src/utils/utils_test.c
@@ -4,8 +4,16 @@
 #include "utils.h"
 
 int test_ll2string(void) {
-    //type 5
-    
+    char buf[100];
+    assert(ll2string(buf, 100, 1234) == 4);
+    return 1;
+}
+
+int test_string2ll(void) {
+    char* src = "1234";
+    long long ll = 0;
+    assert(string2ll(src, 4, &ll) == 1);
+    assert(ll == 1234);
     return 1;
 }
 
@@ -18,6 +26,8 @@ int test_api(void) {
         #endif
         test_cond("ll2string function", 
             test_ll2string() == 1);
+        test_cond("string2ll function",
+            test_string2ll() == 1);
     } test_report()
     return 1;
 }
