18aa93ad6aefe1b0085eef19fc9fd816d8a17d8d
gd.zhou <gd.zhou@trip.com>
2024-09-18 18:51:38 +0800

    [latte] dict_p,ugin  add dictCharKeyCompare

--- src/dict_plugins/dict_plugins.c
+++ src/dict_plugins/dict_plugins.c
@@ -34,3 +34,23 @@ void *dictSdsDup(dict *d, const void *key) {
     UNUSED(d);
     return sdsdup((const sds) key);
 }
+
+int dictCharKeyCompare(void* privdata, const void *key1,
+    const void *key2) {
+    int l1, l2;
+    DICT_NOTUSED(privdata);
+    l1 = strlen(key1);
+    l2 = strlen(key2);
+    if (l1 != l2) return 0;
+    return memcmp(key1, key2, l1) == 0;
+}
+
+int dictCharKeyCompare(void* privdata, const void *key1,
+    const void *key2) {
+    int l1, l2;
+    DICT_NOTUSED(privdata);
+    l1 = strlen(key1);
+    l2 = strlen(key2);
+    if (l1 != l2) return 0;
+    return memcmp(key1, key2, l1) == 0;
+}
