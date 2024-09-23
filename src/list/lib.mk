ddcc66b4df3b71d35d0d91b7dd0ad0035b5012b1
gd.zhou <gd.zhou@trip.com>
2024-09-24 00:28:09 +0800

    add list test

--- src/list/lib.mk
+++ src/list/lib.mk
@@ -1,6 +1,8 @@
 
 
 ifneq ($(USED_LIST), yes) 
+include $(WORKSPACE)/src/zmalloc/lib.mk
+include $(WORKSPACE)/src/iterator/lib.mk
 FINAL_CC_CFLAGS+= -I../list
 ../list/list.o:
 	cd ../list && make list.o
