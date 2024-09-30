ba2f55fc2545228f046b2d94bff296398e5b101e
gd.zhou <gd.zhou@trip.com>
2024-09-30 16:22:57 +0800

    [feature]add vector module

---
+++ src/vector/vector.h
@@ -0,0 +1,26 @@
+#ifndef __LATTE_VECTOR_H
+#define __LATTE_VECTOR_H
+#include <stdlib.h>
+#include <stdio.h>
+#include "iterator/iterator.h"
+/**
+ *  非线程安全的
+ */
+typedef struct vector {
+    void **data;
+    size_t capacity;
+    size_t count;
+} vector;
+vector* vectorCreate();
+void vectorRelease(vector* v);
+void vectorResize(vector* v, size_t new_capacity);
+int vectorPush(vector* v, void* element);
+void* vectorPop(vector* v);
+size_t vectorSize(vector* v);
+void* vectorGet(const vector* v, size_t index);
+void vectorSet(vector* v, size_t index, void* element);
+
+Iterator* vectorGetIterator(vector* v);
+
+
+#endif
