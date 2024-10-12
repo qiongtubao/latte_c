ea2af31536b35086c5c55dd4e5d66687b93b1349
gd.zhou <gd.zhou@trip.com>
2024-10-12 12:58:27 +0800

    [rename] value module

--- src/value/value.h
+++ src/value/value.h
@@ -7,7 +7,7 @@
 #include "dict/dict.h"
 
 
-typedef enum valueType {
+typedef enum value_type_e {
     VALUE_UNDEFINED,
     VALUE_SDS,
     VALUE_INT,
@@ -16,9 +16,9 @@ typedef enum valueType {
     VALUE_BOOLEAN,
     VALUE_ARRAY,
     VALUE_MAP,
-} valueType;
-typedef struct value {
-    valueType type;
+} value_type_e;
+typedef struct {
+    value_type_e type;
     union {
         sds sds_value;
         int64_t i64_value;
@@ -28,39 +28,41 @@ typedef struct value {
         vector* array_value;
         dict* map_value;
     } value;
-} value;
+} value_t;
 
-value* valueCreate();
-void valueRelease(value* v);
+value_t* value_new();
+void value_delete(value_t* v);
 
 
-sds valueGetSds(value* v);
-int64_t valueGetInt64(value* v);
-uint64_t valueGetUInt64(value* v);
-long double valueGetLongDouble(value* v);
-bool valueGetBool(value* v);
-vector* valueGetArray(value* v);
-dict* valueGetMap(value* v);
+sds value_get_sds(value_t* v);
+int64_t value_get_int64(value_t* v);
+uint64_t value_get_uint64(value_t* v);
+long double value_get_longdouble(value_t* v);
+bool value_get_bool(value_t* v);
+vector* value_get_array(value_t* v);
+dict* value_get_map(value_t* v);
 
-void valueSetSds(value* v, sds s);
-void valueSetInt64(value* v, int64_t l);
-void valueSetUInt64(value* v, uint64_t ull);
-void valueSetLongDouble(value* v, long double d);
-void valueSetBool(value* v, bool b);
-void valueSetArray(value* v, vector* ve);
-void valueSetMap(value* v, dict* d);
+sds value_get_binary(value_t* v);
+
+void value_set_sds(value_t* v, sds s);
+void value_set_int64(value_t* v, int64_t l);
+void value_set_uint64(value_t* v, uint64_t ull);
+void value_set_longdouble(value_t* v, long double d);
+void value_set_bool(value_t* v, bool b);
+void value_set_array(value_t* v, vector* ve);
+void value_set_map(value_t* v, dict* d);
 
 // 1 success 0 fail
-int valueSetBinary(value* v, valueType type,char* s, int len);
-sds valueGetBinary(value* v);
-#define valueIsInt64(v) (v != NULL && v->type == VALUE_INT)
-#define valueIsUInt64(v) (v != NULL && v->type == VALUE_UINT)
-#define valueIsLongDouble(v) (v!= NULL && v->type == VALUE_DOUBLE)
-#define valueIsBool(v) (v != NULL && v->type == VALUE_BOOLEAN)
-#define valueIsSds(v)  (v != NULL && v->type == VALUE_SDS)
-#define valueIsArray(v) (v != NULL && v->type == VALUE_ARRAY)
-#define valueIsMap(v) (v != NULL &&  v->type == VALUE_MAP)
+int value_set_binary(value_t* v, value_type_e type,char* s, int len);
 
-#define valueIsNull(v) (v == NULL || v->type == VALUE_UNDEFINED)
+#define value_is_int64(v) (v != NULL && v->type == VALUE_INT)
+#define value_is_uint64(v) (v != NULL && v->type == VALUE_UINT)
+#define value_is_longdouble(v) (v!= NULL && v->type == VALUE_DOUBLE)
+#define value_is_bool(v) (v != NULL && v->type == VALUE_BOOLEAN)
+#define value_is_sds(v)  (v != NULL && v->type == VALUE_SDS)
+#define value_is_array(v) (v != NULL && v->type == VALUE_ARRAY)
+#define value_is_map(v) (v != NULL &&  v->type == VALUE_MAP)
+
+#define value_is_null(v) (v == NULL || v->type == VALUE_UNDEFINED)
 
 #endif
