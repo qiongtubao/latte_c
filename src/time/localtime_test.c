4df017b7b073bcb9469c2c7d97374507abf43a05
gd.zhou <gd.zhou@trip.com>
2025-07-22 16:24:40 +0800

    [time] add time module

---
+++ src/time/localtime_test.c
@@ -0,0 +1,40 @@
+
+#include "../test/testhelp.h"
+#include "../test/testassert.h"
+#include <unistd.h>
+#include "localtime.h"
+#include <stdio.h>
+
+int test_nolocks_localtime() {
+    /* Obtain timezone and daylight info. */
+    tzset(); /* Now 'timezome' global is populated. */
+    time_t t = time(NULL);
+    struct tm *aux = localtime(&t);
+    int daylight_active = aux->tm_isdst;
+
+    struct tm tm;
+    char buf[1024];
+
+    nolocks_localtime(&tm,t,timezone,daylight_active);
+    strftime(buf,sizeof(buf),"%d %b %H:%M:%S",&tm);
+    printf("[timezone: %d, dl: %d] %s\n", (int)timezone, (int)daylight_active, buf);
+    return 1;
+}
+
+
+int test_api(void) {
+    {
+        #ifdef LATTE_TEST
+            // ..... private
+        #endif
+        test_cond("about nolocks_localtime function", 
+            test_nolocks_localtime() == 1);
+        
+    } test_report()
+    return 1;
+}
+
+int main() {
+    test_api();
+    return 0;
+}
